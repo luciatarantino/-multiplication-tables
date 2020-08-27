d8966542f6844fb2b1b600540a90ae057f7a0ec9
luciatarantino <69126448+luciatarantino@users.noreply.github.com>
2020-08-27 23:52:45 +0200

    Add files via upload

---
+++ script/miojquery.js
@@ -0,0 +1,322 @@
+$(document).ready(function(){//tutto ciò che viene dopo, lo legge alla fine del caricamento della pagina
+    
+    $("ul li:first-child .cor").click(function(){
+        //console.log("sono entrato");
+        $("ul li:first-child .cor").css("background-color","#B2FF59");
+        $("ul li:first-child .cor").css("border","solid 3px #33691E");
+        $("ul li:first-child img").fadeIn(500)
+    });//qui si chiude l'azione .click
+    
+    $("ul li:first-child .sba.uno").click(function(){
+        //console.log("andiamo");
+        $("ul li:first-child .sba.uno").css("background-color","#F44336");
+        $("ul li:first-child .sba.uno").css("border","solid 3px #D32F2F");
+        $("ul li:first-child .sba.uno").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    $("ul li:first-child .sba.due").click(function(){
+        $("ul li:first-child .sba.due").css("background-color","#F44336");
+        $("ul li:first-child .sba.due").css("border","solid 3px #D32F2F");
+        $("ul li:first-child .sba.due").html("NO");  
+    });//qui si chiude l'azione .click
+    
+        $("ul li:first-child .sba.tre").click(function(){
+        $("ul li:first-child .sba.tre").css("background-color","#F44336");
+        $("ul li:first-child .sba.tre").css("border","solid 3px #D32F2F");
+        $("ul li:first-child .sba.tre").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    
+    
+    
+    $("ul li:nth-of-type(2) .cor").click(function(){
+        //console.log("sono entrato");
+        $("ul li:nth-of-type(2) .cor").css("background-color","#B2FF59");
+        $("ul li:nth-of-type(2) .cor").css("border","solid 3px #33691E");
+        $("ul li:nth-of-type(2) img").fadeIn(500)
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(2) .sba.uno").click(function(){
+        //console.log("andiamo");
+        $("ul li:nth-of-type(2) .sba.uno").css("background-color","#F44336");
+        $("ul li:nth-of-type(2) .sba.uno").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(2) .sba.uno").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(2) .sba.due").click(function(){
+        $("ul li:nth-of-type(2) .sba.due").css("background-color","#F44336");
+        $("ul li:nth-of-type(2) .sba.due").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(2) .sba.due").html("NO");  
+    });//qui si chiude l'azione .click
+    
+        $("ul li:nth-of-type(2) .sba.tre").click(function(){
+        $("ul li:nth-of-type(2) .sba.tre").css("background-color","#F44336");
+        $("ul li:nth-of-type(2) .sba.tre").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(2) .sba.tre").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    
+    
+    
+    $("ul li:nth-of-type(3) .cor").click(function(){
+        //console.log("sono entrato");
+        $("ul li:nth-of-type(3) .cor").css("background-color","#B2FF59");
+        $("ul li:nth-of-type(3) .cor").css("border","solid 3px #33691E");
+        $("ul li:nth-of-type(3) img").fadeIn(500)
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(3) .sba.uno").click(function(){
+        //console.log("andiamo");
+        $("ul li:nth-of-type(3) .sba.uno").css("background-color","#F44336");
+        $("ul li:nth-of-type(3) .sba.uno").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(3) .sba.uno").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(3) .sba.due").click(function(){
+        $("ul li:nth-of-type(3) .sba.due").css("background-color","#F44336");
+        $("ul li:nth-of-type(3) .sba.due").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(3) .sba.due").html("NO");  
+    });//qui si chiude l'azione .click
+    
+        $("ul li:nth-of-type(3) .sba.tre").click(function(){
+        $("ul li:nth-of-type(3) .sba.tre").css("background-color","#F44336");
+        $("ul li:nth-of-type(3) .sba.tre").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(3) .sba.tre").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    
+    
+    
+    $("ul li:nth-of-type(4) .cor").click(function(){
+        //console.log("sono entrato");
+        $("ul li:nth-of-type(4) .cor").css("background-color","#B2FF59");
+        $("ul li:nth-of-type(4) .cor").css("border","solid 3px #33691E");
+        $("ul li:nth-of-type(4) img").fadeIn(500)
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(4) .sba.uno").click(function(){
+        //console.log("andiamo");
+        $("ul li:nth-of-type(4) .sba.uno").css("background-color","#F44336");
+        $("ul li:nth-of-type(4) .sba.uno").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(4) .sba.uno").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(4) .sba.due").click(function(){
+        $("ul li:nth-of-type(4) .sba.due").css("background-color","#F44336");
+        $("ul li:nth-of-type(4) .sba.due").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(4) .sba.due").html("NO");  
+    });//qui si chiude l'azione .click
+    
+        $("ul li:nth-of-type(4) .sba.tre").click(function(){
+        $("ul li:nth-of-type(4) .sba.tre").css("background-color","#F44336");
+        $("ul li:nth-of-type(4) .sba.tre").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(4) .sba.tre").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    
+    
+    $("ul li:nth-of-type(5) .cor").click(function(){
+        //console.log("sono entrato");
+        $("ul li:nth-of-type(5) .cor").css("background-color","#B2FF59");
+        $("ul li:nth-of-type(5) .cor").css("border","solid 3px #33691E");
+        $("ul li:nth-of-type(5) img").fadeIn(500)
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(5) .sba.uno").click(function(){
+        //console.log("andiamo");
+        $("ul li:nth-of-type(5) .sba.uno").css("background-color","#F44336");
+        $("ul li:nth-of-type(5) .sba.uno").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(5) .sba.uno").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(5) .sba.due").click(function(){
+        $("ul li:nth-of-type(5) .sba.due").css("background-color","#F44336");
+        $("ul li:nth-of-type(5) .sba.due").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(5) .sba.due").html("NO");  
+    });//qui si chiude l'azione .click
+    
+        $("ul li:nth-of-type(5) .sba.tre").click(function(){
+        $("ul li:nth-of-type(5) .sba.tre").css("background-color","#F44336");
+        $("ul li:nth-of-type(5) .sba.tre").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(5) .sba.tre").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    
+    
+    $("ul li:nth-of-type(6) .cor").click(function(){
+        //console.log("sono entrato");
+        $("ul li:nth-of-type(6) .cor").css("background-color","#B2FF59");
+        $("ul li:nth-of-type(6) .cor").css("border","solid 3px #33691E");
+        $("ul li:nth-of-type(6) img").fadeIn(500)
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(6) .sba.uno").click(function(){
+        //console.log("andiamo");
+        $("ul li:nth-of-type(6) .sba.uno").css("background-color","#F44336");
+        $("ul li:nth-of-type(6) .sba.uno").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(6) .sba.uno").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(6) .sba.due").click(function(){
+        $("ul li:nth-of-type(6) .sba.due").css("background-color","#F44336");
+        $("ul li:nth-of-type(6) .sba.due").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(6) .sba.due").html("NO");  
+    });//qui si chiude l'azione .click
+    
+        $("ul li:nth-of-type(6) .sba.tre").click(function(){
+        $("ul li:nth-of-type(6) .sba.tre").css("background-color","#F44336");
+        $("ul li:nth-of-type(6) .sba.tre").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(6) .sba.tre").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    
+    
+    $("ul li:nth-of-type(7) .cor").click(function(){
+        //console.log("sono entrato");
+        $("ul li:nth-of-type(7) .cor").css("background-color","#B2FF59");
+        $("ul li:nth-of-type(7) .cor").css("border","solid 3px #33691E");
+        $("ul li:nth-of-type(7) img").fadeIn(500)
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(7) .sba.uno").click(function(){
+        //console.log("andiamo");
+        $("ul li:nth-of-type(7) .sba.uno").css("background-color","#F44336");
+        $("ul li:nth-of-type(7) .sba.uno").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(7) .sba.uno").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(7) .sba.due").click(function(){
+        $("ul li:nth-of-type(7) .sba.due").css("background-color","#F44336");
+        $("ul li:nth-of-type(7) .sba.due").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(7) .sba.due").html("NO");  
+    });//qui si chiude l'azione .click
+    
+        $("ul li:nth-of-type(7) .sba.tre").click(function(){
+        $("ul li:nth-of-type(7) .sba.tre").css("background-color","#F44336");
+        $("ul li:nth-of-type(7) .sba.tre").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(7) .sba.tre").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    
+    
+    $("ul li:nth-of-type(8) .cor").click(function(){
+        //console.log("sono entrato");
+        $("ul li:nth-of-type(8) .cor").css("background-color","#B2FF59");
+        $("ul li:nth-of-type(8) .cor").css("border","solid 3px #33691E");
+        $("ul li:nth-of-type(8) img").fadeIn(500)
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(8) .sba.uno").click(function(){
+        //console.log("andiamo");
+        $("ul li:nth-of-type(8) .sba.uno").css("background-color","#F44336");
+        $("ul li:nth-of-type(8) .sba.uno").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(8) .sba.uno").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(8) .sba.due").click(function(){
+        $("ul li:nth-of-type(8) .sba.due").css("background-color","#F44336");
+        $("ul li:nth-of-type(8) .sba.due").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(8) .sba.due").html("NO");  
+    });//qui si chiude l'azione .click
+    
+        $("ul li:nth-of-type(8) .sba.tre").click(function(){
+        $("ul li:nth-of-type(8) .sba.tre").css("background-color","#F44336");
+        $("ul li:nth-of-type(8) .sba.tre").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(8) .sba.tre").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    
+    
+    $("ul li:nth-of-type(9) .cor").click(function(){
+        //console.log("sono entrato");
+        $("ul li:nth-of-type(9) .cor").css("background-color","#B2FF59");
+        $("ul li:nth-of-type(9) .cor").css("border","solid 3px #33691E");
+        $("ul li:nth-of-type(9) img").fadeIn(500)
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(9) .sba.uno").click(function(){
+        //console.log("andiamo");
+        $("ul li:nth-of-type(9) .sba.uno").css("background-color","#F44336");
+        $("ul li:nth-of-type(9) .sba.uno").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(9) .sba.uno").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    $("ul li:nth-of-type(9) .sba.due").click(function(){
+        $("ul li:nth-of-type(9) .sba.due").css("background-color","#F44336");
+        $("ul li:nth-of-type(9) .sba.due").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(9) .sba.due").html("NO");  
+    });//qui si chiude l'azione .click
+    
+        $("ul li:nth-of-type(9) .sba.tre").click(function(){
+        $("ul li:nth-of-type(9) .sba.tre").css("background-color","#F44336");
+        $("ul li:nth-of-type(9) .sba.tre").css("border","solid 3px #D32F2F");
+        $("ul li:nth-of-type(9) .sba.tre").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    
+    
+    $("ul li:last-of-type .cor").click(function(){
+        //console.log("sono entrato");
+        $("ul li:last-of-type .cor").css("background-color","#B2FF59");
+        $("ul li:last-of-type .cor").css("border","solid 3px #33691E");
+        $("ul li:last-of-type img").fadeIn(500)
+    });//qui si chiude l'azione .click
+    
+    $("ul li:last-of-type .sba.uno").click(function(){
+        //console.log("andiamo");
+        $("ul li:last-of-type .sba.uno").css("background-color","#F44336");
+        $("ul li:last-of-type .sba.uno").css("border","solid 3px #D32F2F");
+        $("ul li:last-of-type .sba.uno").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    $("ul li:last-of-type .sba.due").click(function(){
+        $("ul li:last-of-type .sba.due").css("background-color","#F44336");
+        $("ul li:last-of-type .sba.due").css("border","solid 3px #D32F2F");
+        $("ul li:last-of-type .sba.due").html("NO");  
+    });//qui si chiude l'azione .click
+    
+        $("ul li:last-of-type .sba.tre").click(function(){
+        $("ul li:last-of-type .sba.tre").css("background-color","#F44336");
+        $("ul li:last-of-type .sba.tre").css("border","solid 3px #D32F2F");
+        $("ul li:last-of-type .sba.tre").html("NO");  
+    });//qui si chiude l'azione .click
+    
+    
+    
+    
+  
+    
+    
+});//qui si chiude l'azione .ready
+    
+    
+    
+    
+    
+    
+    /*   
+    $("ul li:first-child li.sba:nth-of-type(1)").click(function(){
+        console.log("primo sbagliato")
+        $("ul li:first-child li.sba:nth-of-type(1)").css("background-color","#F44336");
+        $("ul li:first-child li.sba:nth-of-type(1)").css("border","solid 3px #D32F2F");
+        $("ul li:first-child li.sba:nth-of-type(1)").html("NO");  
+    });//qui si chiude l'azione .click
+    
+           
+    $("ul li:first-child li.sba:nth-of-type(2)").click(function(){
+        console.log("sono entrato");
+        //$("ul li:first-child li.sba:nth-child(2)").css("background-color","#F44336");
+       // $("ul li:first-child li.sba:nth-child(2)").css("border","solid 3px #D32F2F");
+        //$("ul li:first-child li.sba:nth-child(2)").html("NO");  
+    });//qui si chiude l'azione .click
+    */
+
+    /*
+  var Ab=$("ul li:first-child .sba.due");
+    
+    Ab.click(function(){
+        Ab.css("background-color","#F44336");
+        Ab.css("border","solid 3px #D32F2F");
+        Ab.html("NO");          
+    });//qui si chiude l'azione .click
+    */
